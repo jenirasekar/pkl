936cbddb3ad13a79ae41bae48d6727a740c37cf9
Jenira Sekar <jenirasekar@gmail.com>
2022-12-08 14:17:33 +0700

    add d-none for priv access

--- sekar-crud/asset/js/script.js
+++ sekar-crud/asset/js/script.js
@@ -2,6 +2,6 @@ var accessList = document.getElementsByClassName("access");
 
 for (var access of accessList) {
 	if (access.innerHTML == "private") {
-		access.parentNode.classList = '';
+		access.parentNode.classList = 'd-none';
 	}
 }
