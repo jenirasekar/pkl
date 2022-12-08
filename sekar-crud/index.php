2475597b51cf9b37c4ce250dcd552c1cb050d134
rahrmny12 <rahmatprayogo12@gmail.com>
2022-12-08 11:22:35 +0700

    fitur private pake js

--- sekar-crud/index.php
+++ sekar-crud/index.php
@@ -58,9 +58,9 @@ if (isset($_POST['cari'])) {
 							<td><?= $i ?></td>
 							<td><?= $row["nama_doc"] ?></td>
 							<td><?= $row["file_doc"]  ?></td>
-							<td><?= $row['access'] ?></td>
+							<td class="access"><?= $row['access'] ?></td>
 							<!-- tampilin user yang nambahin docnya -->
-							<td></td>
+							<!-- <td></td> -->
 							<td>
 								<a href="ubah.php?id=<?= $row["id"]; ?>">Ubah</a> |
 								<a href="hapus.php?id=<?= $row["id"]; ?>" onclick="return confirm('Apakah Anda Yakin?');">Hapus</a>
@@ -79,7 +79,7 @@ if (isset($_POST['cari'])) {
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 
-	<!-- <script src="asset/js/script.js"></script> -->
+	<script src="asset/js/script.js"></script>
 </body>
 
 </html>
